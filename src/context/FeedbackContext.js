bc2310cb1e738f96a91558161a12a5f7e7edf2c2
vermarahul86 <i.vermarahul@gmail.com>
2023-02-16 00:13:47 +0530

    40.Link to json backend server and fetch data from the db.json file created.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
@@ -7,25 +7,21 @@ const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
 
-    const [feedback, setFeedback] = useState(
-        [
-            {
-                id : 1,
-                text : 'This is feedback item 1',
-                rating : 10
-            },
-            {
-                id : 2,
-                text : 'This is feedback item 2',
-                rating : 8
-            },
-            {
-                id : 3,
-                text : 'This is feedback item 3',
-                rating : 5
-            }
-        ]
-    )
+    const[isLoading, setIsLoading] = useState(true)
+
+    const [feedback, setFeedback] = useState([])
+
+    useEffect(()=>{
+        fetchFeedback()
+    }, [])
+
+    //Fetch Feedback from the json-server
+    const fetchFeedback = async() =>{
+        const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+        const data  = await response.json()
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item : {},
@@ -61,6 +57,7 @@ export const FeedbackProvider = ({children}) => {
     return <FeedbackContext.Provider value= {{
         feedback,
         feedbackEdit, 
+        isLoading,
         deleteFeedback, 
         addFeedback, 
         editFeedback,
