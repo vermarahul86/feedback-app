ab60a12b2531776a2071a235d96af399df0de5a4
vermarahul86 <i.vermarahul@gmail.com>
2023-02-21 23:51:54 +0530

    42.Add feedback and set proxy.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -17,7 +17,7 @@ export const FeedbackProvider = ({children}) => {
 
     //Fetch Feedback from the json-server
     const fetchFeedback = async() =>{
-        const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+        const response = await fetch('/feedback?_sort=id&_order=desc')
         const data  = await response.json()
         setFeedback(data)
         setIsLoading(false)
@@ -44,9 +44,19 @@ export const FeedbackProvider = ({children}) => {
         }
     }
 
-    const addFeedback = (newFeedback) =>{
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback,...feedback])
+    const addFeedback = async(newFeedback) => {
+        
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+           body : JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+        //newFeedback.id = uuidv4()
+        setFeedback([data,...feedback])
 
     }
 
